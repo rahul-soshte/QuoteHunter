e4c2378ccb9eb3efc2dfdffabf854e36265ff05b
rahul-soshte <rahul.soshte47@gmail.com>
2017-08-25 21:54:19 +0530

    Image Loading Solved:Removed Picasso Used Glide

--- app/src/main/java/com/example/hunter/motiva/MainActivity2.java
+++ app/src/main/java/com/example/hunter/motiva/MainActivity2.java
@@ -9,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -39,7 +40,8 @@ public class MainActivity2 extends AppCompatActivity {
     Button imageButton;
 DatabaseReference rootRef,demoRef;
 TextView textView;
-long n;
+static long n;
+    String value;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +49,9 @@ long n;
         setContentView(R.layout.activity_main2);
         imageView = (ImageView) findViewById(R.id.imageView);
         imageButton = (Button) findViewById(R.id.imageButton2);
-        textView=(TextView)findViewById(R.id.url);
+        textView = (TextView) findViewById(R.id.url);
 
-      //  Random random=new Random();
+        //  Random random=new Random();
         //Toast.makeText(this,Integer.toString(random.nextInt(45)),Toast.LENGTH_LONG).show();
 
         //database reference pointing to root of database
@@ -61,7 +63,7 @@ long n;
                 rootRef.addValueEventListener(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        n=dataSnapshot.getChildrenCount();
+                        n = dataSnapshot.getChildrenCount();
                     }
 
                     @Override
@@ -72,19 +74,26 @@ long n;
 
 
                 Random r = new Random();
-                long number = (long)(r.nextDouble()*n);
+                long number = (long) (r.nextDouble() * n);
 
                 //Random random1=new Random();
                 //Integer i=random1.nextLong((int)n);
-               // long v = ThreadLocalRandom.current().nextLong(n);
+                // long v = ThreadLocalRandom.current().nextLong(n);
 
-                demoRef=rootRef.child(Long.toString(number));
+                demoRef = rootRef.child(Long.toString(number));
                 demoRef.child("img_url").addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
-                        String value = dataSnapshot.getValue(String.class);
-                        textView.setText(value);
+                        value = dataSnapshot.getValue(String.class);
+
+                        //Picasso.with(MainActivity2.this)
+                          //      .load(value)
+                            //    .into(imageView);
+                        Glide.with(MainActivity2.this).load(value).into(imageView);
+                      //  Toast.makeText(MainActivity2.this,value,Toast.LENGTH_LONG).show();
+                        //textView.setText(value);
                     }
+
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
                     }
@@ -94,7 +103,30 @@ long n;
         });
     }
 
+        @Override
+        protected void onStart() {
+            super.onStart();
+        //    https://firebasestorage.googleapis.com/v0/b/moti-a1b1f.appspot.com/o/2.jpg?alt=media&token=c241ecfd-5cbd-40dc-8fe5-0b45d1c9b0c6
+         //   String url="https://firebasestorage.googleapis.com/v0/b/moti-a1b1f.appspot.com/o/2.jpg?alt=media&token=c241ecfd-5cbd-40dc-8fe5-0b45d1c9b0c6";
+
+            //Picasso.with(MainActivity2.this)
+              //      .load(url)
+                //    .into(imageView);
+
+            String url="https://firebasestorage.googleapis.com/v0/b/moti-a1b1f.appspot.com/o/1.jpg?alt=media&token=762ccf25-4fc3-4f16-a611-5061f6c6bb10";
+            //Picasso.with(MainActivity2.this)
+              //      .load(url)
+                //    .into(imageView);
+            Glide.with(this).load(url).into(imageView);
+//            String url="https://firebasestorage.googleapis.com/v0/b/moti-a1b1f.appspot.com/o/2.jpg?alt=media&token=c241ecfd-5cbd-40dc-8fe5-0b45d1c9b0c6";
+
+            //Toast.makeText(this,value,Toast.LENGTH_LONG).show();
+        }
+       // Picasso.with(MainActivity2.this)
+         //       .load(value)
+           //     .into(imageView);
+
+    }
 
 
-}
 
