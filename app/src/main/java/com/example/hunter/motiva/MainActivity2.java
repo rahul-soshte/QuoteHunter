f1d5e70bbc60c2b6093aacb2e7ecc62724bbff4b
rahul-soshte <rahul.soshte47@gmail.com>
2017-08-29 00:03:41 +0530

    Removed HttpHandler

--- app/src/main/java/com/example/hunter/motiva/MainActivity2.java
+++ app/src/main/java/com/example/hunter/motiva/MainActivity2.java
@@ -1,6 +1,6 @@
 package com.example.hunter.motiva;
 
-import android.os.AsyncTask;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,36 +8,14 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
-
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.squareup.picasso.Picasso;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ThreadLocalRandom;
-
 //https://stackoverflow.com/questions/42186839/how-to-get-a-random-object-from-firebase-database-in-android
 public class MainActivity2 extends AppCompatActivity {
     ImageView imageView;
