6c1fb80308ab61fd95922dace3a2d573a725b272
rahul-soshte <rahul.soshte47@gmail.com>
2017-09-12 20:24:15 +0530

    Toast Added

--- app/src/main/java/com/hunterlab/hunter/motiva/MainActivity2.java
+++ app/src/main/java/com/hunterlab/hunter/motiva/MainActivity2.java
@@ -1,6 +1,9 @@
 package com.hunterlab.hunter.motiva;
 
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,6 +11,8 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.bumptech.glide.Glide;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -46,40 +51,57 @@ ProgressBar progressBar;
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                if (isOnline()) {
+                    progressBar.setVisibility(View.VISIBLE);
+                    hunter.setVisibility(View.GONE);
+                    rootRef.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            n = dataSnapshot.getChildrenCount();
+                        }
 
-                progressBar.setVisibility(View.VISIBLE);
-                hunter.setVisibility(View.GONE);
-                rootRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        n = dataSnapshot.getChildrenCount();
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
 
-                    }
-                });
+                        }
+                    });
 
 
-                Random r = new Random();
-                long number = (long) (r.nextDouble() * n);
+                    Random r = new Random();
+                    long number = (long) (r.nextDouble() * n);
 
-                demoRef = rootRef.child(Long.toString(number));
-                demoRef.child("img_url").addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        value = dataSnapshot.getValue(String.class);
+                    demoRef = rootRef.child(Long.toString(number));
+                    demoRef.child("img_url").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            value = dataSnapshot.getValue(String.class);
 
-                     Glide.with(getApplicationContext())
-                             .load(value)
-                             .into(imageView);
-                    }
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
+                            Glide.with(getApplicationContext())
+                                    .load(value)
+                                    .into(imageView);
+                        }
 
-                    }
-                });
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                else{
+
+                    Toast.makeText(getApplicationContext(),"Not Connected to Internet!",Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+
     }
