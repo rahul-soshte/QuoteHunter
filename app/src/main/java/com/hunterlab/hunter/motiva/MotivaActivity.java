951be38f9405d9733382a0a09f761e757a09a52e
rahul-soshte <rahul.soshte47@gmail.com>
2017-10-04 16:11:07 +0530

    Converted MainActivity2 to Random Fragment

--- app/src/main/java/com/hunterlab/hunter/motiva/MotivaActivity.java
+++ app/src/main/java/com/hunterlab/hunter/motiva/MotivaActivity.java
@@ -1,5 +1,8 @@
 package com.hunterlab.hunter.motiva;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,7 +17,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MotivaActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,9 @@ public class MotivaActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        FragmentTransaction ft = getFragmentManager().beginTransaction();
+        ft.replace(R.id.container,new RandomFragment(), "visible_fragment");
+        ft.commit();
     }
 
     @Override
@@ -79,15 +85,23 @@ public class MotivaActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        Fragment fragment=null;
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            fragment=new RandomFragment();
         } else if (id == R.id.nav_gallery) {
-
-        }else if(id==R.id.nav_share)
-        {
-
+            fragment=new FamousFragment();
         }
+
+        if(fragment!=null) {
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.container, fragment, "visible_fragment");
+           // ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            ft.commit();
+        }
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
---
+++ app/src/main/java/com/hunterlab/hunter/motiva/RandomFragment.java
@@ -0,0 +1,212 @@
+package com.hunterlab.hunter.motiva;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Random;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link RandomFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link RandomFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RandomFragment extends Fragment {
+
+    ImageView imageView;
+    Button nextButton;
+    DatabaseReference rootRef,demoRef;
+    TextView textView;
+
+    static long n;
+    String value;
+    ProgressBar progressBar;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public RandomFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RandomFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RandomFragment newInstance(String param1, String param2) {
+        RandomFragment fragment = new RandomFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)
+    {
+
+        super.onActivityCreated(savedInstanceState);
+        imageView = (ImageView) getActivity().findViewById(R.id.imageView);
+        nextButton = (Button) getActivity().findViewById(R.id.imageButton2);
+        textView = (TextView) getActivity().findViewById(R.id.url);
+        progressBar=(ProgressBar)getActivity().findViewById(R.id.progress);
+
+        nextButton.setBackgroundColor(Color.WHITE);
+        progressBar.setVisibility(View.GONE);
+        //database reference pointing to root of database
+        rootRef = FirebaseDatabase.getInstance().getReference();
+
+        Glide.with(getActivity().getApplicationContext())
+                .load(R.drawable.hunter)
+                .into(imageView);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (isOnline()) {
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    rootRef.addValueEventListener(new ValueEventListener() {
+
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            n = dataSnapshot.getChildrenCount();
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+                    Random r = new Random();
+                    long number = (long) (r.nextDouble() * n);
+                    demoRef = rootRef.child(Long.toString(number));
+                    demoRef.child("img_url").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            value = dataSnapshot.getValue(String.class);
+
+                            Glide.with(getActivity().getApplicationContext())
+                                    .load(value)
+                                    .into(imageView);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(getActivity().getApplicationContext(),"Not Connected to Internet!",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_random, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager)getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
